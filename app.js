956a8ea46b536be3cc535a38abee528586b10012
elzod <you@.com>
2023-10-15 17:54:34 +0500

    jvs

---
+++ app.js
@@ -0,0 +1,21 @@
+let  red = document.querySelector(".red")
+red.addEventListener("click", function () {
+    document.querySelector("body").style.background = "red";
+  });
+let  yellow = document.querySelector(".yellow")
+yellow.addEventListener("click", function () {
+    document.querySelector("body").style.background = "yellow";
+  });
+let  blue = document.querySelector(".blue")
+blue.addEventListener("click", function () {
+    document.querySelector("body").style.background = "blue";
+  });
+let  black = document.querySelector(".black")
+black.addEventListener("click", function () {
+    document.querySelector("body").style.background = "black";
+  });
+let  start = document.querySelector(".start")
+start.addEventListener("click", function () {
+    document.querySelector("body").style.background = "white";
+  });
+  
